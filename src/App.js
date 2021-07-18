7284729938840a0c0a708e78067ab854c06e0fcb
yjcho <jinjanic91@gmail.com>
2021-07-18 16:42:08 +0900

    about computed

--- src/App.js
+++ src/App.js
@@ -4,55 +4,66 @@ import './App.css';
 import { observer } from 'mobx-react';
 import React, { useContext } from 'react';
 import PersonContext from './contexts/PersonContext';
-import autobind from 'autobind-decorator';
-
-@observer
-class App extends React.Component {
-  static contextType = PersonContext;
-
-  render() {
-    const personStore = this.context;
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> 나이: {personStore.age} </p>
-          <p>
-            <button onClick={this.click}>plus</button>
-          </p>
-        </header>
-      </div>
-    );
-  }
-  @autobind
-  click() {
-    const personStore = this.context;
-    personStore.plusAge();
-  }
-}
+import { computed } from 'mobx';
+// import autobind from 'autobind-decorator';
 
 // 함수형
-// function App() {
-//   const personStore = useContext(PersonContext);
+function App() {
+  console.log('rendered');
+  const personStore = useContext(PersonContext);
 
-//   function click() {
+  // computed를 함수형으로 사용 (decorator 사용안함)
+  const age10 = computed(() => {
+    return Math.floor(personStore.age / 10) * 10;
+  }).get();
+
+  function click() {
+    personStore.plusAge();
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* class형 computed 사용 */}
+        {/* <p> 나이: {personStore.age10} </p> */}
+        {/* 함수형 computed */}
+        <p> 나이: {age10} </p>
+        <p>
+          <button onClick={click}>plus</button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default observer(App);
+
+// class 형
+// @observer
+// class App extends React.Component {
+//   static contextType = PersonContext;
+
+//   render() {
+//     const personStore = this.context;
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p> 나이: {personStore.age} </p>
+//           <p>
+//             <button onClick={this.click}>plus</button>
+//           </p>
+//         </header>
+//       </div>
+//     );
+//   }
+//   @autobind
+//   click() {
+//     const personStore = this.context;
 //     personStore.plusAge();
 //   }
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p> 나이: {personStore.age} </p>
-//         <p>
-//           <button onClick={click}>plus</button>
-//         </p>
-//       </header>
-//     </div>
-//   );
 // }
 
-// export default observer(App);
-
-export default App;
+// export default App;
